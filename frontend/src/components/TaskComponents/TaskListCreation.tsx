b6a75df973ec66df6608bcc33934c7ad896fea86
Pontus A <pontus.abrahamsson@iths.se>
2023-11-30 15:18:42 +0100

    Add new button text to task submit button

--- frontend/src/components/TaskComponents/TaskListCreation.tsx
+++ frontend/src/components/TaskComponents/TaskListCreation.tsx
@@ -128,7 +128,7 @@ const TaskListCreation = () => {
             <button
               type="submit"
               className="w-full mt-6 mb-3 bg-indigo-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">
-              Login
+              add new task
             </button>
           </form>
         </div>
