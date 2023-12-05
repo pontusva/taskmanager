7bd6a0c674f23880b30231b95df5ce82dd6f05ef
Pontus A <pontus.abrahamsson@iths.se>
2023-12-05 09:15:32 +0100

    Add npm npm script for tailwind with cypress

--- frontend/cypress/component/NavigationComponent.cy.tsx
+++ frontend/cypress/component/NavigationComponent.cy.tsx
@@ -1,9 +1,9 @@
 import Navigation from '../../src/Navigation';
-import Login from '../../src/pages/LoginPage';
 import { BrowserRouter as Router } from 'react-router-dom';
+import '../../dist/index.css'; //https://stackoverflow.com/questions/70306004/running-cypress-tests-with-tailwindcss-3
 
 describe('NavigationComponent.cy.tsx', () => {
-  it('playground', () => {
+  it('Check if Sign In text is showing when entering first page', () => {
     cy.mount(
       <Router>
         <Navigation />
